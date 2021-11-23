8f5b19697208d63bf4dc3f002a5f6f19e169e25f
nrandell <nick@nickrandell.com>
2021-11-23 13:43:36 +0000

    fix: more lottie

--- src/elements/LottieElement.tsx
+++ src/elements/LottieElement.tsx
@@ -60,7 +60,7 @@ const RealLottieElement: React.FC<Props> = (props) => {
         if (isVisible) {
           if (!state.loaded) {
             debug && console.log("Loading");
-            import(/* webpackLoad: false */ "./LottieExport")
+            import(/* webpackLoad: false */ "lottie-web")
               .then((Lottie) => {
                 if (!state.cancelled) {
                   const animationData = JSON.parse(
