cf3201e30c959c51819e265826345d46b3352e3d
nrandell <nick@nickrandell.com>
2022-01-25 18:44:15 +0000

    fix: tweaks to svgicon positioning

--- src/elements/SvgIcon.tsx
+++ src/elements/SvgIcon.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { CSSProperties, useMemo } from "react";
 import { VisualComponentProps } from ".";
-import { useStyles } from "..";
 
 interface Props extends Partial<VisualComponentProps> {
   encoded: string;
@@ -8,10 +7,45 @@ interface Props extends Partial<VisualComponentProps> {
 }
 
 export const SvgIcon: React.FC<Props> = (props) => {
-  const { encoded, alt, className } = props;
-  const fullStyles = useStyles(props);
+  const {
+    encoded,
+    alt,
+    className,
+    objectFit,
+    objectPosition,
+    fitParent,
+    noStyle,
+    style,
+  } = props;
+
+  const fullStyles: CSSProperties | undefined = useMemo(() => {
+    if (noStyle) {
+      return undefined;
+    }
+    const conditional: CSSProperties = fitParent
+      ? {
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          left: "0",
+          top: "0",
+        }
+      : {};
+    return { ...conditional, ...style };
+  }, [fitParent, noStyle, style]);
 
   return (
-    <img src={encoded} alt={alt} className={className} style={fullStyles} />
+    <div className={className} style={fullStyles}>
+      <img
+        src={encoded}
+        alt={alt}
+        style={{
+          height: "100%",
+          width: "100%",
+          objectFit,
+          objectPosition,
+        }}
+      />
+    </div>
   );
 };
